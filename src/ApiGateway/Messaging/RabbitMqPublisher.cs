bf7eae0387d37f3b1f5b587b48d09e899e438add
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-16 13:09:53 -0500

    commit - apigateway/messaging/RabbitMqPublisher :.

--- src/ApiGateway/Messaging/RabbitMqPublisher.cs
+++ src/ApiGateway/Messaging/RabbitMqPublisher.cs
@@ -1,19 +1,53 @@
-﻿using Microsoft.EntityFrameworkCore.Metadata;
-using RabbitMQ.Client;
+﻿using RabbitMQ.Client;
+using Microsoft.Extensions.Configuration;
 using System.Text;
 using System.Text.Json;
-
 
 namespace AdvancedMicroservicesSolution.src.ApiGateway.Messaging
 {
     public class RabbitMqPublisher : IDisposable
     {
         private readonly IConnection _connection;
-        private readonly IModel _channel;
+        private readonly IChannel _channel;
         private readonly IConfiguration _config;
+
+        public RabbitMqPublisher(IConfiguration config)
+        {
+            _config = config;
+
+            var host = _config["RabbitMQ:Host"] ?? throw new ArgumentNullException("RabbitMQ:Host");
+            var user = _config["RabbitMQ:User"] ?? throw new ArgumentNullException("RabbitMQ:User");
+            var pass = _config["RabbitMQ:Password"] ?? throw new ArgumentNullException("RabbitMQ:Password");
+            var exchange = _config["RabbitMQ:Exchange"] ?? throw new ArgumentNullException("RabbitMQ:Exchange");
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = host,
+                UserName = user,
+                Password = pass
+            };
+
+            // Nueva API (asincrónica) en RabbitMQ.Client 7+
+            _connection = factory.CreateConnectionAsync().GetAwaiter().GetResult();
+            _channel = _connection.CreateChannelAsync().GetAwaiter().GetResult();
+
+            _channel.ExchangeDeclareAsync(exchange, ExchangeType.Fanout, durable: true).GetAwaiter().GetResult();
+        }
+
+        public void Publish<T>(T message, string routingKey = "")
+        {
+            var exchange = _config["RabbitMQ:Exchange"] ?? throw new ArgumentNullException("RabbitMQ:Exchange");
+            var json = JsonSerializer.Serialize(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            _channel.BasicPublishAsync(exchange, routingKey, body).GetAwaiter().GetResult();
+        }
+
         public void Dispose()
         {
-            throw new NotImplementedException();
+            _channel?.Dispose();
+            _connection?.Dispose();
+            GC.SuppressFinalize(this);
         }
     }
 }
