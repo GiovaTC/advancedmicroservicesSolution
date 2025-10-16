1f0b9758a3cf8c6a357099fc9dcd7be48e6ed25a
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-16 11:52:18 -0500

    commit - apigateway/messaging/RabbitMqPublisher :.

--- src/ApiGateway/Messaging/RabbitMqPublisher.cs
+++ src/ApiGateway/Messaging/RabbitMqPublisher.cs
@@ -1,6 +1,15 @@
-﻿namespace AdvancedMicroservicesSolution.src.ApiGateway.Messaging
+﻿using RabbitMQ.Client;
+using System.Text;
+using System.Text.Json;
+
+
+namespace AdvancedMicroservicesSolution.src.ApiGateway.Messaging
 {
-    public class RabbitMqPublisher
+    public class RabbitMqPublisher : IDisposable
     {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
