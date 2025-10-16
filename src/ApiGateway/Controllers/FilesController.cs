137f319dbab3f988e996c0aadb253e3a924134cd
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-16 11:31:25 -0500

    commit - apigateway/controllers/FilesController .

--- src/ApiGateway/Controllers/FilesController.cs
+++ src/ApiGateway/Controllers/FilesController.cs
@@ -1,6 +1,31 @@
-﻿namespace AdvancedMicroservicesSolution.src.ApiGateway.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using AdvancedMicroservicesSolution.src.ApiGateway.Services;
+
+namespace AdvancedMicroservicesSolution.src.ApiGateway.Controllers
 {
-    public class FilesController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FilesController : ControllerBase
     {
+        private readonly IFileService _fileService;
+
+        public FilesController(IFileService fileService) => _fileService = fileService;
+
+        [HttpPost]
+        [Authorize]
+        [RequestSizeLimit(10_485_760)] // Limit to 10 MB]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            try
+            {
+                var saved = await _fileService.SaveFileAsync(file);
+                return Ok(new { fileName = saved });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { error = ex.Message });
+            }
+        }
     }
 }
