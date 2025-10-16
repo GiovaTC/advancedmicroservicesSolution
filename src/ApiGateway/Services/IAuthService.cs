7eff899869c174ff36da8825802c736c57cc6735
gtapiero <giovanny.tapiero@pi.edu.co>
2025-10-16 10:10:58 -0500

    commit - apigateway/services/IAuthService :.

--- src/ApiGateway/Services/IAuthService.cs
+++ src/ApiGateway/Services/IAuthService.cs
@@ -1,6 +1,10 @@
-﻿namespace AdvancedMicroservicesSolution.src.ApiGateway.Services
+﻿//using AdvancedMicroservicesSolution.src.ApiGateway.DTOs;
+
+namespace AdvancedMicroservicesSolution.src.ApiGateway.Services
 {
     public interface IAuthService
     {
+        string GenerateToken(string userId, string role);
+        Task<bool> ValidateUserAsync(string username, string password);
     }
 }
